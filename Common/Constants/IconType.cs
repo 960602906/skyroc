c2a4309e1b210258fab6fe598f618540992e73ab
MrQin <960602906@qq.com>
2025-12-01 17:18:37 +0800

    feat: add IconType enum for icon type definitions

---
+++ Common/Constants/IconType.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Constants;
+
+/// <summary>
+///  图标类型枚举
+/// </summary>
+public enum IconType
+{
+    /// <summary>
+    /// Iconify
+    /// </summary>
+    Iconify = 1,
+    /// <summary>
+    /// 本地
+    /// </summary>
+    Local = 2
+}
