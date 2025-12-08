8fdc41ce52eb9d0dcecf63a5e0ab95b883dea4c0
MrQin <960602906@qq.com>
2025-12-08 16:43:36 +0800

    版本变更

---
+++ Infrastructure/Migrations/20251208065542_init9.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "leader_name",
+                table: "sys_department",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "leader_name",
+                table: "sys_department");
+        }
+    }
+}
