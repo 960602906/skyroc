40c23d5d09e557932061caeacef0168aea6e9072
MrQin <960602906@qq.com>
2025-12-12 18:01:24 +0800

    redis

---
+++ Application/interfaces/ITokenCacheService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.Auth;
+
+namespace Application.interfaces;
+
+public interface ITokenCacheService
+{
+    Task SaveTokenAsync(string token, TokenCacheDataDto data, TimeSpan? expiration = null);
+    Task<TokenCacheDataDto?> GetTokenDataAsync(string token);
+    Task<bool> IsTokenValidAsync(string token);
+    Task RevokeTokenAsync(string token);
+    Task RevokeAllUserTokensAsync(int userId);
+    Task<List<TokenCacheDataDto>> GetUserActiveTokensAsync(int userId);
+}
